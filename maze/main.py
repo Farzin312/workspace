eac3433f96f34702f90e03d1d38fa55fc8f22a9d
Farzin312 <farzinbusiness0@gmail.com>
2023-12-28 21:17:22 -0500

    update

--- maze/main.py
+++ maze/main.py
@@ -1,1 +1,47 @@
+from tkinter import Tk, BOTH, Canvas
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.__canvas = Canvas(self.__root, bg = "white", height = height, width = width)
+        self.__canvas.pack(fill = BOTH, expand=1)
+        self.__running = False
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+    
+    def draw_line(self, line, fill_color = "black"):
+        line.draw(self.__canvas, fill_color)
+    
+    def close(self):
+        self.running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+    def draw(self,canvas, fill_color="black"):
+        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill = fill_color, width = 2)
+        canvas.pack(fill = BOTH, expand = 1)
+def main():
+    win = Window(800,600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
+    win.wait_for_close()
+
+main()
+
+    
 
